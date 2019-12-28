fcd230c94be38a5cd737871a7ccdf53c84af446f
Nick Saxelby <mail@nsaxelby.com>
2019-12-28 18:53:54 +0000

    #1 Initial GUI and show my IP address

---
+++ EasyFileSender-Windows/EFS.WindowsFormApp/EFSForm.cs
@@ -0,0 +1,38 @@
+﻿using EFS.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFS.WindowsFormApp
+{
+    public partial class EFSForm : Form
+    {
+        public EFSForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EFSForm_Load(object sender, EventArgs e)
+        {
+            LoadIpAddressLabel();
+        }
+
+        private void LoadIpAddressLabel()
+        {
+            try
+            {
+                yourIPLabel.Text = EnvironmentTools.GetMyIP4IpAddress();
+            }
+            catch
+            {
+                yourIPLabel.Text = "Unknown - Error getting IPV4";
+            }
+        }
+    }
+}
