b233754af6745f236dbfc7d691491815d6af5d5d
nsaxelby <mail@nsaxelby.com>
2020-01-12 14:09:27 +0000

    #4 Added draggable areas (#11)
    Supports multiple files

--- EasyFileSender-Windows/EFS.WindowsFormApp/EFSForm.cs
+++ EasyFileSender-Windows/EFS.WindowsFormApp/EFSForm.cs
@@ -94,12 +94,14 @@ namespace EFS.WindowsFormApp
                 {
                     selectedIpLabel.Text = "You: " + clientObjectSelected.IpAddress + " - Receiving Files";
                     sendFileButton.Visible = false;
+                    dragAndDropPanel.Visible = false;
                     transfersPanel.Size = new System.Drawing.Size(transfersPanel.Size.Width, this.Size.Height - 89);
                 }
                 else
                 {
                     selectedIpLabel.Text = clientObjectSelected.IpAddress;
                     sendFileButton.Visible = true;
+                    dragAndDropPanel.Visible = true;
                     transfersPanel.Size = new System.Drawing.Size(transfersPanel.Size.Width, this.Size.Height - 118);
                 }
             }
@@ -115,18 +117,14 @@ namespace EFS.WindowsFormApp
             using (OpenFileDialog ofd = new OpenFileDialog())
             {
                 if (ofd.ShowDialog() == DialogResult.OK)
-                {
-                    long fileSizeBytes = 0;
-                    fileSizeBytes = new FileInfo(ofd.FileName).Length;
-
+                {                  
                     try
                     {
-                        Guid guid = Guid.NewGuid();
-                        _sendFileService.StartNewTransferThread(guid, selectedIpLabel.Text, ofd.FileName, fileSizeBytes, OnFileTransferStatusChanged);
+                        CommonTransfer(ofd.FileName);
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show("Error Starting Transfer: " + ex.Message);
+                        MessageBox.Show("Error Starting Transfer: " + ex.Message, "Transfer Error");
                     }
                 }
             }
@@ -135,6 +133,42 @@ namespace EFS.WindowsFormApp
         private void OnFileTransferStatusChanged(FileTransferStatus fileTransferStatus)
         {
             Debug.WriteLine("Transfer guid: " + fileTransferStatus.TransferID.ToString() + " + file : " + fileTransferStatus.SourceFile + " percentage: " + fileTransferStatus.Progress.ToString("00.00"));
+        }
+
+        private void DragAndDropPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                try
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    foreach (var FilesDroppedName in files)
+                    {
+                        Guid returnedGuid = CommonTransfer(FilesDroppedName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error Starting Transfer: " + ex.Message, "Transfer Error");
+                }
+            }
+        }
+
+        private Guid CommonTransfer(string fileName)
+        {
+            long fileSizeBytes = 0;
+            fileSizeBytes = new FileInfo(fileName).Length;
+            Guid guid = Guid.NewGuid();
+            _sendFileService.StartNewTransferThread(guid, selectedIpLabel.Text, fileName, fileSizeBytes, OnFileTransferStatusChanged);
+            return guid;
+        }
+
+        private void DragAndDropPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            { 
+                e.Effect = DragDropEffects.Copy;
+            }
         }
     }
 }
