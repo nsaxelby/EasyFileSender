7462519e07bcada1d5b4d1c4a4c6bd31667f5acf
nsaxelby <mail@nsaxelby.com>
2020-01-18 12:46:36 +0000

    #13 Added queueing up of transfers, also added status colors in UI (#14)

--- EasyFileSender-Windows/EFS.Utilities/FileTransfer/FileSendTransferThread.cs
+++ EasyFileSender-Windows/EFS.Utilities/FileTransfer/FileSendTransferThread.cs
@@ -37,37 +37,9 @@ namespace EFS.Utilities.FileTransfer
             };
         }
 
+        // Must be a non blocking call to release the UI [#13]
         public bool StartTransfer()
         {
-            string destinationFileURI = "ftp://" + _destinationIP + "/" + Path.GetFileName(_sourceFile);
-
-            // Get the object used to communicate with the server.
-            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(destinationFileURI);
-            request.Credentials = new NetworkCredential("anonymous", "anon@anon.com");
-            request.Method = WebRequestMethods.Ftp.GetFileSize;
-
-            bool fileAlreadyExists = false;
-
-            try
-            {
-                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-                fileAlreadyExists = true;
-            }
-            catch (WebException ex)
-            {
-                FtpWebResponse response = (FtpWebResponse)ex.Response;
-                if (response.StatusCode == FtpStatusCode.ActionNotTakenFileUnavailable)
-                {
-                    //Does not exist
-                    fileAlreadyExists = false;
-                }
-            }
-
-            if (fileAlreadyExists)
-            {
-                throw new FileAlreadyExistsException("File : " + Path.GetFileName(_sourceFile) + " already exists on destination.");
-            }
-
             _transferThread = new Thread(new ThreadStart(DoTransfer));
             _transferThread.Start();
             return true;
@@ -79,7 +51,35 @@ namespace EFS.Utilities.FileTransfer
             {
                 string destinationFileURI = "ftp://" + _destinationIP + "/" + Path.GetFileName(_sourceFile);
 
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(destinationFileURI);
+                // Get the object used to communicate with the server.
+                FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(destinationFileURI);
+                request.Credentials = new NetworkCredential("anonymous", "anon@anon.com");
+                request.Method = WebRequestMethods.Ftp.GetFileSize;
+
+                bool fileAlreadyExists = false;
+
+                try
+                {
+                    FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                    fileAlreadyExists = true;
+                }
+                catch (WebException ex)
+                {
+                    FtpWebResponse response = (FtpWebResponse)ex.Response;
+                    if (response.StatusCode == FtpStatusCode.ActionNotTakenFileUnavailable)
+                    {
+                        //Does not exist
+                        fileAlreadyExists = false;
+                    }
+                }
+
+                if (fileAlreadyExists)
+                {
+                    throw new FileAlreadyExistsException("File : " + Path.GetFileName(_sourceFile) + " already exists on destination.");
+                }
+
+
+                request = (FtpWebRequest)WebRequest.Create(destinationFileURI);
                 request.Method = WebRequestMethods.Ftp.UploadFile;
                 request.Credentials = new NetworkCredential("anonymous", "anon@anon.com");
 
